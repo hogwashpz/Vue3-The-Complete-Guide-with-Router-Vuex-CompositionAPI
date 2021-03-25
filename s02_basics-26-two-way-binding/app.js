bae67511468103b6ea293e8813aae4ffae25cbcb
hogwashpz <m.hogwash89@yahoo.com>
2021-03-25 22:13:08 +0100

    v-model & computed properties

--- s02_basics-26-two-way-binding/app.js
+++ s02_basics-26-two-way-binding/app.js
@@ -5,16 +5,37 @@ const app = Vue.createApp({
       name: ''
     };
   },
+  computed: {
+    fullname() {
+      console.debug('Computed ran...');
+      if (this.name === '')
+        return '';
+      else
+        return this.name + ' ' + 'Žolnaj'; 
+    }
+  },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    outputFullname() {
+      console.debug('Running again...');
+      if (this.name === '')
+        return '';
+      else
+        return this.name + ' ' + 'Žolnaj';
     },
+    // this allways adding second name to data prop "name"
+    // use two-way bidning
+    setName(event) {
+      this.name = event.target.value + ' Žolnaj';
+    },
+
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
-      // this.counter--;
+    },
+    resetInput() {
+      this.name = '';
     }
   }
 });
